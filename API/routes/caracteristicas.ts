d2f0636895eca945ce8784dd8ff423e616548bf6
Gabriel Caldeira Medeiros <gabrielcaldeiramedeiros1999@gmail.com>
2025-08-27 16:33:49 -0300

    Add routas de armamentos, caracteristicas e requerimentos

---
+++ API/routes/caracteristicas.ts
@@ -0,0 +1,87 @@
+import { Alcance, PrismaClient, Recarga } from '@prisma/client'
+import { Router } from 'express'
+import { z } from 'zod'
+
+const prisma = new PrismaClient()
+
+const router = Router()
+
+const caracteristicaSchema = z.object({
+     nome: z.string(
+        { message: "O nome deve possuir só caractere string e no mínimo 3 de caractere" }).min(3),
+    descricao: z.string(
+        { message: "A descrição deve possuir só caractere string e no mínimo 2 de caractere" }).min(2),
+    armamentoId: z.number().int().nonnegative(
+        { message: "ArmamentoId obrigatório e deve ser número inteiro positivo"}),
+})
+
+router.get("/", async (req, res) => {
+    try {
+        const caracteristicas = await prisma.caracteristica.findMany({
+        include: {
+        armamento: true,
+       }
+        })
+        res.status(200).json(caracteristicas)
+    } catch (error) {
+        res.status(500).json({ erro: error })
+    }
+})
+
+router.post("/", async (req, res) => {
+
+    const valida = caracteristicaSchema.safeParse(req.body)
+    if (!valida.success) {
+        res.status(400).json({ erro: valida.error })
+        return
+    }
+
+    const { nome, descricao, armamentoId } = valida.data
+
+    try {
+      const caracteristica = await prisma.caracteristica.create({
+        data: { nome, descricao, armamentoId }
+      })
+      res.status(201).json(caracteristica)
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params
+  
+    try {
+      const caracteristica = await prisma.caracteristica.delete({
+        where: { id: Number(id) }
+      })
+      res.status(200).json(caracteristica)
+    } catch (error) {
+      res.status(400).json({ erro: error })
+    }
+  })
+
+  
+router.put("/:id", async (req, res) => {
+    const { id } = req.params
+
+    const valida = caracteristicaSchema.safeParse(req.body)
+    if (!valida.success) {
+        res.status(400).json({ erro: valida.error })
+        return
+    }
+
+    const { nome, descricao,  armamentoId } = valida.data
+
+    try {
+        const caracteristica = await prisma.caracteristica.update({
+            where: { id: Number(id)},
+            data: { nome, descricao, armamentoId }
+        })
+        res.status(200).json(caracteristica)
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+})
+
+export default router
---
+++ API/routes/requerimentos.ts
@@ -0,0 +1,86 @@
+import { Alcance, PrismaClient, Recarga } from '@prisma/client'
+import { Router } from 'express'
+import { z } from 'zod'
+
+const prisma = new PrismaClient()
+
+const router = Router()
+
+const requerimentoSchema = z.object({
+     atributo: z.string(
+        { message: "O atributo deve possuir só caractere string e no mínimo 3 de caractere" }).min(3),
+    valor: z.number().int(
+        { message: "O valor deve possuir apenas numeros inteiros positivos e valor maior do que  zero" }).gte(1),
+    armamentoId: z.number().int().nonnegative(
+        { message: "PersonagemId obrigatório e deve ser número inteiro positivo"}),
+})
+
+router.get("/", async (req, res) => {
+    try {
+        const requerimentos = await prisma.requerimento.findMany({
+        include: {
+        armamento: true,
+       }
+        })
+        res.status(200).json(requerimentos)
+    } catch (error) {
+        res.status(500).json({ erro: error })
+    }
+})
+
+router.post("/", async (req, res) => {
+
+    const valida = requerimentoSchema.safeParse(req.body)
+    if (!valida.success) {
+        res.status(400).json({ erro: valida.error })
+        return
+    }
+
+    const { atributo, valor, armamentoId } = valida.data
+
+    try {
+      const requerimento = await prisma.requerimento.create({
+        data: { atributo, valor, armamentoId }
+      })
+      res.status(201).json(requerimento)
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params
+  
+    try {
+      const requerimento = await prisma.requerimento.delete({
+        where: { id: Number(id) }
+      })
+      res.status(200).json(requerimento)
+    } catch (error) {
+      res.status(400).json({ erro: error })
+    }
+  })
+
+router.put("/:id", async (req, res) => {
+    const { id } = req.params
+
+    const valida = requerimentoSchema.safeParse(req.body)
+    if (!valida.success) {
+        res.status(400).json({ erro: valida.error })
+        return
+    }
+
+    const { atributo, valor, armamentoId } = valida.data
+
+    try {
+        const requerimento = await prisma.requerimento.update({
+            where: { id: Number(id)},
+            data: { atributo, valor, armamentoId }
+        })
+        res.status(200).json(requerimento)
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+})
+
+export default router
